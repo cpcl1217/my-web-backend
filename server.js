4768f8de8535ccd1efa7136a6d4e576154d59fb5
Mark <136040246+cpcl1217@users.noreply.github.com>
2024-05-21 16:15:49 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-// server.js
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
@@ -14,8 +14,10 @@ const io = socketIo(server, {
 
 const port = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(cors({
-  origin: 'https://my-website-672ojlw7dq-de.a.run.app/'
+  origin: 'https://localhost:3000'
 }));
 
 io.on('connection', (socket) => {
@@ -23,7 +25,7 @@ io.on('connection', (socket) => {
 
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg); // 添加调试日志
-    io.emit('chat message', msg);
+    io.emit('chat message', { message: msg, id: socket.id }); // 包含发送者的 ID
   });
 
   socket.on('disconnect', () => {
